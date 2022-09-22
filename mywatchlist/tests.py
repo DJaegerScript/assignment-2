4c0990c653c6ee20d6167d0213ee5c55137ce9ce
DJaegerScript <adjiedjaka0902@gmail.com>
2022-09-22 08:27:29 +0700

    fixed image path

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
 # Create your tests here.
+class WatchListTest(TestCase):
+    def test_response_code_200(self):
+        watch_list_resp = self.client.get('/mywatchlist/')
+        watch_list_html_resp = self.client.get('/mywatchlist/html')
+        watch_list_xml_resp = self.client.get('/mywatchlist/xml')
+        watch_list_json_resp = self.client.get('/mywatchlist/json')
+
+        self.assertEqual(watch_list_resp.status_code, 200)
+        self.assertEqual(watch_list_html_resp.status_code, 200)
+        self.assertEqual(watch_list_xml_resp.status_code, 200)
+        self.assertEqual(watch_list_json_resp.status_code, 200)
+        
+    def test_response_code_200_parameterized_json_endpoint(self):
+        response_codes = []
+        for i in range(10):
+            response = self.client.get('/mywatchlist/json/' + str(i+1))
+            response_codes.append(response.status_code)
+        self.assertTrue(all(response_code == 200 for response_code in response_codes))
+        
+    def test_response_code_200_parameterized_xml_endpoint(self):
+        response_codes = []
+        for i in range(10):
+            response = self.client.get('/mywatchlist/xml/' + str(i+1))
+            response_codes.append(response.status_code)
+        self.assertTrue(all(response_code == 200 for response_code in response_codes))
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,8 +8,8 @@ app_name = 'mywatchlist'
 urlpatterns= [
     path('', show_watchlist, name="show_watchlist"),
     path('html', show_watchlist, name="show_watchlist"),
-    path('xml/', show_xml, name='show_xml'),
+    path('xml', show_xml, name='show_xml'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
-    path('json/', show_json, name='show_json'),
+    path('json', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
 ]
